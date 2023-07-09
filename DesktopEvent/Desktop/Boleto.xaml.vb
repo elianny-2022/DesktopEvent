e1fa24b02af0909a831fd64e314d6f7349ab5c7f
elianny199920@gmail.com <104779804+elianny-2022@users.noreply.github.com>
2023-07-09 00:04:49 -0400

    Commit

---
+++ DesktopEvent/Desktop/Boleto.xaml.vb
@@ -0,0 +1,65 @@
+﻿Imports Newtonsoft.Json
+Imports System.Net.Http
+Imports System.Text
+
+Public Class Boleto
+    Public Property Evento As Evento
+
+    ' Método para guardar los boletos
+    Private Async Sub GuardarDetalles_Click(sender As Object, e As RoutedEventArgs)
+        ' Obtener el nombre del evento desde la ventana principal
+
+        If Evento Is Nothing Then
+            MessageBox.Show("No se ha asignado ningún evento.")
+            Return
+        End If
+
+        Dim eventoId As Long = Evento.eventoId
+        Dim nombreEvento As String = txtNombreEventoTB.Text
+        Dim cantidadBoletos As Double
+        Dim precio As Double
+        ' Crear una instancia de Boletos con los datos ingresados en la interfaz
+        Dim boletos As New Boletos()
+        boletos.nombreEvento = txtNombreEventoTB.Text
+        boletos.precio = If(Double.TryParse(txtPrecioTB.Text, precio), precio, 0.0)
+        boletos.cantidadBoletos = If(Double.TryParse(txtCantidadBoletosTB.Text, cantidadBoletos), cantidadBoletos, 0.0)
+        boletos.asiento = CType(cmbTipoAsientoTB.SelectedItem, ComboBoxItem).Content.ToString()
+
+
+        Dim boleto As New Boletos()
+        boletos.eventoId = eventoId
+
+        ' Serializar el objeto boletos a formato JSON
+        Dim jsonBoletos As String = JsonConvert.SerializeObject(boletos)
+
+        ' Crear el contenido de la solicitud HTTP
+        Dim content As New StringContent(jsonBoletos, Encoding.UTF8, "application/json")
+
+        Try
+            ' Enviar la solicitud POST para guardar los boletos
+            Using httpClient As New HttpClient()
+                Dim response As HttpResponseMessage = Await httpClient.PostAsync($"http://localhost:8080/boletos/boleto/save", content)
+
+                ' Verificar si la solicitud fue exitosa
+                If response.IsSuccessStatusCode Then
+                    MessageBox.Show("Boletos guardados exitosamente")
+                    Close()
+                Else
+                    MessageBox.Show("Error al guardar los boletos")
+                End If
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Error en la solicitud HTTP: " & ex.Message)
+        End Try
+    End Sub
+End Class
+
+
+Public Class Boletos
+
+    Public Property eventoId As Long
+    Public Property nombreEvento As String
+    Public Property cantidadBoletos As Double
+    Public Property precio As Double
+    Public Property asiento As String
+End Class
---
+++ DesktopEvent/Desktop/Event.xaml.vb
@@ -0,0 +1,64 @@
+﻿Imports System.Net.Http
+Imports System.Text
+Imports Newtonsoft.Json
+
+Class Eventos
+    Private txtNombreEvento As Object
+    Private txtTipoEvento As Object
+    Private dpFecha As Object
+    Private txtUbicacion As Object
+    Private txtDescripcion As Object
+
+    Private Async Sub GuardarEvento_Click(sender As Object, e As RoutedEventArgs)
+        Dim evento As New Evento()
+
+        evento.nombreEvento = txtNombreEventoTB.Text
+        evento.tipoEvento = txtTipoEventoTB.Text
+        evento.fecha = dpFechaTB.SelectedDate
+        evento.ubicacion = txtUbicacionTB.Text
+        evento.descripcion = txtDescripcionTB.Text
+
+        Using client As New HttpClient()
+            ' Configurar la URL base de tu API
+            client.BaseAddress = New Uri("http://localhost:8080/")
+
+            ' Serializar el objeto de evento a JSON
+            Dim jsonEvento As String = JsonConvert.SerializeObject(evento)
+            Dim content = New StringContent(jsonEvento, Encoding.UTF8, "application/json")
+
+            ' Realizar la solicitud POST al endpoint de la API para guardar el evento
+            Dim response = Await client.PostAsync("/eventos/evento/save", content)
+
+            If response.IsSuccessStatusCode Then
+                MessageBox.Show("El evento ha sido guardado correctamente.")
+                Dim boletosWindow As New Boleto()
+                boletosWindow.txtNombreEventoTB.Text = txtNombreEventoTB.Text
+                boletosWindow.Evento = evento
+                boletosWindow.Show()
+                Close()
+            Else
+                MessageBox.Show("No se pudo guardar el evento. Inténtalo nuevamente.")
+            End If
+        End Using
+    End Sub
+
+
+    Private Sub LimpiarCampos()
+        txtNombreEvento.Text = ""
+        txtTipoEvento.Text = ""
+        dpFecha.SelectedDate = Date.Now
+        txtUbicacion.Text = ""
+        txtDescripcion.Text = ""
+    End Sub
+End Class
+
+
+Public Class Evento
+    Public Property eventoId As Long
+    Public Property nombreEvento As String
+    Public Property tipoEvento As String
+    Public Property fecha As Date?
+    Public Property ubicacion As String
+    Public Property descripcion As String
+End Class
+
